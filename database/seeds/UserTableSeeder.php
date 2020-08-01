31c0e5a599850231a5aab6def13eb5e9e0abb546
SawanEranga <sekandamby@gmail.com>
2020-08-01 11:46:58 +0530

    User table seeder commit.

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->delete();
+        DB::table('users')->insert([
+           [
+               'id' => 1,
+               'name' => 'Support Agent 1',
+               'email' => 'supportagent@test.com',
+               'password' => bcrypt('WB5m3Gr85j)ew&')
+           ]
+        ]);
+    }
+}
